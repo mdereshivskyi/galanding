8968880cd60fa8390b69d8ba33a02030361d519e
mdereshivskyi <m.dereshivskyi@gmail.com>
2019-07-08 12:06:01 +0300

    third commit

---
+++ js/common.js
@@ -0,0 +1,56 @@
+$(document).ready(function() {
+
+	$("#portfolio_grid").mixItUp();
+
+	$(".popup").magnificPopup({type:"image"});
+	$(".popup_content").magnificPopup({
+		type:"inline",
+		midClick: true
+	});
+
+	function heightDetect() {
+		$(".main_head").css("height", $(window).height());
+	};
+	heightDetect();
+	$(window).resize(function() {
+		heightDetect();
+	});
+
+	$(".toggle_mnu").click(function() {
+		$(".sandwich").toggleClass("active");
+	});
+
+	$(".top_mnu ul a").click(function() {
+		$(".top_mnu").fadeOut(600);
+		$(".sandwich").toggleClass("active");
+		$(".top_text").css("opacity", "1");
+	}).append("<span>");
+
+	$(".toggle_mnu").click(function() {
+		if ($(".top_mnu").is(":visible")) {
+			$(".top_text").css("opacity", "1");
+			$(".top_mnu").fadeOut(600);
+			$(".top_mnu li a").removeClass("fadeInUp animated");
+		} else {
+			$(".top_text").css("opacity", ".1");
+			$(".top_mnu").fadeIn(600);
+			$(".top_mnu li a").addClass("fadeInUp animated");
+		};
+	});
+
+	$(".portfolio_item").each(function(i) {
+		$(this).find("a").attr("href", "#work_" + i);
+		$(this).find(".podrt_descr").attr("id", "work_" + i);
+	});
+
+	$("input, select, textarea").jqBootstrapValidation();
+
+	$(".top_mnu ul a").mPageScroll2id();
+
+});
+$(window).load(function() {
+
+	$(".loader_inner").fadeOut();
+	$(".loader").delay(400).fadeOut("slow");
+
+}); 
